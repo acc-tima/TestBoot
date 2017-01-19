aa0671aecdbc0568b3decc335020fccdd67c0df5
ChenYu <tima@126.com>
2017-01-19 19:01:24 +0800

    添加toString equals

--- src/main/java/com/tima/test/dao/entity/User.java
+++ src/main/java/com/tima/test/dao/entity/User.java
@@ -1,11 +1,15 @@
 package com.tima.test.dao.entity;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private Long id;
 
     private String name;
 
     private String password;
+
+    private static final long serialVersionUID = 1L;
 
     public User(Long id, String name, String password) {
         this.id = id;
@@ -39,5 +43,46 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password == null ? null : password.trim();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        User other = (User) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", password=").append(password);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
     }
 }
